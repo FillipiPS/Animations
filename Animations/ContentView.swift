1582afecb298582674e4827d2315680c14a3cca3
FillipiPS <fillipi_ps@outlook.com>
2020-09-09 21:23:10 -0300

    Added Wave Animation that Decreases Opacity

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,8 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount: CGFloat = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Wave Animation")
+                .font(.largeTitle)
+            Spacer()
+            Circle()
+                .frame(width: 100.0, height: 100.0)
+                .overlay(
+                    Circle()
+                        .stroke(Color.black)
+                        .scaleEffect(animationAmount)
+                        .opacity(Double(2 - animationAmount))
+                        .animation(Animation.easeOut(duration: 2).repeatForever(autoreverses: false))
+                )
+                .onAppear {
+                    self.animationAmount = 2
+                }
+            Spacer()
+        }
     }
 }
 
