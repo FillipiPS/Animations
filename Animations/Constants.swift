b74b92f26224dabf1ed13adcde12ac049807b165
FillipiPS <fillipi_ps@outlook.com>
2020-09-16 20:30:35 -0300

    Added Constants With Views Titles

---
+++ Animations/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Animations
+//
+//  Created by Fillipi Paiva Suszek on 16/09/20.
+//  Copyright © 2020 Fillipi Paiva Suszek. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let waveViewTitle = "Wave Animation"
+    static let rotation3DViewTitle = "Rotation 3D Animation"
+    static let FlipCardViewTitle = "Flip Card"
+}
