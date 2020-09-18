1fc610eb030337cb9059df989ff7d95f95de9649
FillipiPS <fillipi_ps@outlook.com>
2020-09-18 20:18:48 -0300

    Added Color Constant

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -28,7 +28,7 @@ struct ContentView: View {
                     Text("Flip Card")
             }
         }
-        .background(Color("BackgroundColor"))
+        .background(Color(K.Color.background))
     }
 }
 
